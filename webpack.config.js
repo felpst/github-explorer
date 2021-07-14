e5f0ef6b07d2bb9385c0c4615fb80e4411759c13
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-07-14 10:47:54 -0300

    Environment fully configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path') // This is a good convention
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'productions'; // Environment variables, that are used to configure things based on the mode that the project is running. This identifies if the product is on development mode or production mode.
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map', // This will allow me to check the original code when there is an error.
+    mode: isDevelopment ? 'development' : 'production', // Making sure I am using the correct mode based on the mode I am running the project.
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map', // This will allow me to check the original code when there is an error.
     entry: path.resolve(__dirname, 'src', 'index.jsx'), //This indicates what is the main file of the project
     output: { // This is the file I am going to generate with weebpack
         path: path.resolve(__dirname, 'dist'),
@@ -27,6 +28,11 @@ module.exports = {
                 test: /\.jsx$/, // This is a regular expression that gets all the files that end with .jsx
                 exclude: /node_modules/, // I don't want that the .jsx files that import get converte
                 use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/, 
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'], // Here I am stating that I want to use both loaders to interpret my css file.
             }
         ],
     }
